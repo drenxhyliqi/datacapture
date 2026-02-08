81af688d6b552277c4bd4463b290ca4612eb3dcf
drenxhyliqi <drenxhyliqi3@gmail.com>
2026-02-08 21:19:42 +0100

    Add small ellipse images to HeroBanner component

--- components/ui/hero-banner.tsx
+++ components/ui/hero-banner.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import insideEllipse from "@/assets/background/insideEllipse.svg";
+import smallEllipse from "@/assets/background/smallEllipse.svg";
 import leftBackground from "@/assets/background/LeftBackground.svg";
 import rightBackground from "@/assets/background/RightBackground.svg";
 
 const ellipseSrc = typeof insideEllipse === "string" ? insideEllipse : (insideEllipse as { src: string }).src;
+const smallEllipseSrc = typeof smallEllipse === "string" ? smallEllipse : (smallEllipse as { src: string }).src;
 const leftBgSrc = typeof leftBackground === "string" ? leftBackground : (leftBackground as { src: string }).src;
 const rightBgSrc = typeof rightBackground === "string" ? rightBackground : (rightBackground as { src: string }).src;
 
@@ -28,6 +30,14 @@ export default function HeroBanner() {
           sizes="(max-width: 1024px) 98vw, (max-width: 1536px) 80.14vw, 75vw"
           priority
         />
+        {/* Small ellipse – bottom-left (inside main ellipse) */}
+        <div className="absolute left-[22%] bottom-[18%] w-4 h-4 z-10">
+          <Image src={smallEllipseSrc} alt="" width={16} height={16} className="w-full h-full object-contain" />
+        </div>
+        {/* Small ellipse – top-right (inside main ellipse) */}
+        <div className="absolute right-[22%] top-[18%] w-4 h-4 z-10">
+          <Image src={smallEllipseSrc} alt="" width={16} height={16} className="w-full h-full object-contain" />
+        </div>
       </div>
     </section>
   );
