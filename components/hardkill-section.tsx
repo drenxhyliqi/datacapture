a0edb1177ae455ed4486aea00c8fe7af968e227f
dajvazi <donart.ajvazi@hotmail.com>
2026-02-10 20:08:21 +0100

    completed hardkill page

--- components/hardkill-section.tsx
+++ components/hardkill-section.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import Image from "next/image"
-import hardkillfeature2 from "@/assets/hardkill-3.png"
+import hardkillfeature2 from "@/assets/hardkill-2.png"
+import hardkillfeature3 from "@/assets/hardkill-3.png"
+import hardkillfeature4 from "@/assets/hardkill-4.png"
 import { GradientCard } from "./ui/GradientCard"
 import DiscussSection from "./discussSection"
 import Slideshow from "./slideshow"
@@ -38,6 +40,14 @@ const operationalModes = [
     }
 ]
 
+const specs = [
+    { label: "CALIBER", value: "NATO 7.62×51mm`" },
+    { label: "CYCLIC RATE OF FIRE (RDS/MIN)", value: "850" },
+    { label: "WEIGHT (W/O AMMO)", value: "300 lbs" },
+    { label: "POINTING ACCURACY (MOA)CALIBER", value: "< 1 MOA" },
+    { label: "DEFEATS", value: "Groups 1–3" },
+  ];
+
 export default function HardkillSection() {
     return (
         <section className="container mx-auto relative w-full py-10 lg:py-32 px-6 lg:px-8">
@@ -54,12 +64,41 @@ export default function HardkillSection() {
                     </div>
                 </div>
 
-                {/* FEATURES */}
+                {/* FEATURES 1*/}
+                <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-8 lg:gap-12 mb-10 mt-20">
+                    <div className="mt-10 space-y-2">
+                        <h2 className="text-3xl text-white font-semibold">
+                            BULLFROG M240
+                        </h2> <br />
+                        <p className="text-base text-white">ENGINEERED FOR AUTONOMOUS PROTECTION AGAINST UAVS</p><br />
+                        <p className="text-base text-white">BULLFROG™ IS A LIGHTWEIGHT, LOW-POWER AUTONOMOUS WEAPON STATION DESIGNED TO DETECT, IDENTIFY, AND NEUTRALIZE ENEMY UXS. ENGINEERED FOR MOBILITY AND VERSATILITY, BULLFROG™ EXCELS IN BOTH ON-THE-MOVE OPERATIONS AND THE PROTECTION OF HIGH-VALUE TARGETS LIKE POWER SUBSTATIONS. CAPABLE OF ENGAGING GROUP 1-3 UAS, IT OFFERS BOTH AUTONOMOUS AND SEMI-AUTONOMOUS MODES, DELIVERING FLEXIBLE, RELIABLE DEFENSE AT EVERY TURN.</p>
+                    </div>
+
+                    {/* IMAGE */}
+                    <div className="w-full flex justify-center lg:justify-start">
+                        <Image
+                        src={hardkillfeature2}
+                        alt="GNNS Product"
+                        className="
+                            object-contain
+                            w-full
+                            max-w-[320px] sm:max-w-[420px] md:max-w-[520px] lg:max-w-[620px]
+                            h-auto
+                        "
+                        />
+                    </div>
+                </div>
+
+                <div className="w-full flex flex-col items-center justify-center lg:justify-start mb-30">
+                    <h1 className="text-5xl text-orange-300 font-semibold">Defeats</h1><br />
+                    <p className="text-base text-3xl text-white">Groups 1–3</p>
+                    <p className="text-base text-3xl text-white">Maximum Effective Range: 800m</p>
+                </div>
               
                 
 
-                {/* FEATURES */}
-                <div className="text-center mb-16 lg:mb-24">
+                {/* FEATURES 2 */}
+                <div className="text-center mb-16 mt-10 mb-24">
                     <h2 className="text-3xl md:text-4xl lg:text-5xl font-[400] text-white mb-6">
                     TRL-9 Combat-Proven Technology
                     </h2>
@@ -73,7 +112,7 @@ export default function HardkillSection() {
                     {/* IMAGE */}
                     <div className="w-full flex justify-center lg:justify-start">
                         <Image
-                        src={hardkillfeature2}
+                        src={hardkillfeature3}
                         alt="GNNS Product"
                         className="
                             object-contain
@@ -101,10 +140,47 @@ export default function HardkillSection() {
                     </div>
                 </div>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-20 sm:gap-6 lg:gap-7">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-30 sm:gap-6 lg:gap-7">
                     {hardkillFeatures.map((feature) => (
                         <HardKillCard key={feature.title} service={feature} />
                     ))}
+                </div>
+
+                {/* Feature 3 */}
+                <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-8 lg:gap-12 mb-6 mt-30">
+                    <div className="mt-10 space-y-2">
+                        <h2 className="text-3xl text-white font-semibold">
+                            System Charateristics
+                        </h2>
+                        {specs.map((s) => (
+                            <div
+                            key={s.label}
+                            className="grid grid-cols-[1fr_auto] items-baseline gap-x-10"
+                            >
+                            <div className="text-2xl text-white uppercase leading-tight">
+                                {s.label}
+                            </div>
+
+                            <div className="text-2xl text-white text-right whitespace-nowrap">
+                                {s.value}
+                            </div>
+                            </div>
+                        ))}
+                    </div>
+
+                    {/* IMAGE */}
+                    <div className="w-full flex justify-center lg:justify-start">
+                        <Image
+                        src={hardkillfeature4}
+                        alt="GNNS Product"
+                        className="
+                            object-contain
+                            w-full
+                            max-w-[320px] sm:max-w-[420px] md:max-w-[520px] lg:max-w-[620px]
+                            h-auto
+                        "
+                        />
+                    </div>
                 </div>
             </div>
         </section>
--- components/hardkillCard.tsx
+++ components/hardkillCard.tsx
@@ -12,9 +12,8 @@ export function HardKillCard({ service }: HardKillCardProps) {
     return (
         <GradientCard>
             <div className="flex h-[17rem] flex-col items-start px-6 py-10 text-white shadow-[0_18px_45px_rgba(0,0,0,0.6)]">
-                {/* Icon with multi-ring circular background */}
                 <div className="mb-1 flex flex-row items-start text-start">
-                    <div className="relative h-20 w-20">
+                    <div className="relative h-15 w-15">
                         <Image
                             src={service.icon}
                             alt={service.title}
@@ -25,7 +24,7 @@ export function HardKillCard({ service }: HardKillCardProps) {
                     </div>
 
                     {/* Title */}
-                    <h3 className="text-white font-bold lg:text-xl">
+                    <h3 className="text-white items-start font-bold lg:text-xl mt-1">
                         {service.title}
                     </h3>
                 </div>
